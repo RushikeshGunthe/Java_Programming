9ad5eb11d682f94e043775b8f1a2063e1ad7add8
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-11-03 12:21:43 +0530

    Add Assignment_19 Q3

--- Assignments/Assignmrnt_19/program19_3.java
+++ Assignments/Assignmrnt_19/program19_3.java
@@ -1,34 +1,28 @@
-class program19_3 
-{
-    public static void main(String[] args) 
-    {
+class program19_3 {
+    public static void main(String[] args) {
         Logic lobj = new Logic();
-        lobj.CheckDivisible(82);
+        lobj.CheckDivisible(55);
     }
 }
 
-class Logic
-{
+class Logic {
 
     ///////////////////////////////////////////////////////////////////////////////////
-    /// 
-    /// Function name:  CheckDivisible
-    /// Input:          Integer
-    /// Output:         None
-    /// Discription:    Accepts a number checks if the number is divisible by 5 and 11.
-    /// Auther:         Rushikesh Vinod Gunthe
-    /// Date:           02/11/2025
-    ///  
+    ///
+    /// Function name: CheckDivisible
+    /// Input: Integer
+    /// Output: None
+    /// Discription: Accepts a number checks if the number is divisible by 5 and 11.
+    /// Auther: Rushikesh Vinod Gunthe
+    /// Date: 02/11/2025
+    ///
     ///////////////////////////////////////////////////////////////////////////////////
-    
-    void CheckDivisible(int iNo)
+
+    void CheckDivisible(int iNo) 
     {
-        if((iNo % 5 == 0) && (iNo % 11 == 0))
-        {
+        if ((iNo % 5 == 0) && (iNo % 11 == 0)) {
             System.out.println(iNo + " is divisible by 5 and 11.");
-        }
-        else
-        {
+        } else {
             System.out.println(iNo + " is not divisible by 5 and 11.");
         }
     }
