2934e5538575a97fa4aef9ba13071abc2ec37c4f
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-11-15 17:36:48 +0530

    Add Assignment22 Q5

---
+++ Assignments/Assignment_22/Assignment22_5.java
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Packages
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+
+class program22_4 {
+
+    public static void main(String args[]) 
+    {
+        int iSize = 0, iRet = 0, iValue = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+
+        iSize = sobj.nextInt();
+
+        ArrayX aobj = new ArrayX(iSize);
+
+        aobj.Accept();
+
+        System.out.println("Enter number to count frequency:");
+        iValue = sobj.nextInt();
+
+        iRet = aobj.Frequency(iValue);
+
+        System.out.println("Frequency of " +iValue + " is: "+ iRet);
+
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Class name:      ArrayX
+// Discription:     Class to allocate memory dynamically of array and accepts its elements from user
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Logic Functions of ArrayX:
+//
+// Function name:   Frequency
+// Discription:     Accepts an number as iNo and counts the frequency of iNo present in the array and returns the count of frequency
+// Input:           Integer
+// Output:          Integer
+//
+// Author:          Rushikesh Vinod Gunthe
+// Date:            15/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ArrayX 
+{
+
+    private int Arr[];
+    private int iSize;
+
+    // Constructor for allocating resources
+    public ArrayX(int iNo) 
+    {
+        iSize = iNo;            //Size of array
+        Arr = new int[iSize];   //Dynamic memory allocation of array
+    }
+
+    //Method to accept elements of array (Getter)
+    public void Accept() 
+    {
+        int iCnt = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Elements of Array:");
+
+        for (iCnt = 0; iCnt < iSize; iCnt++) 
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+    }
+
+    // Method to return count of iNo presents in the array
+    public int Frequency(int iNo) 
+    {
+        int iCnt = 0, iCount = 0;
+
+        for (iCnt = 0; iCnt < iSize; iCnt++) 
+        {
+            if(iNo == Arr[iCnt])
+            {
+                iCount++;
+            }
+        }
+
+        return iCount;
+        
+    }
+
+}
