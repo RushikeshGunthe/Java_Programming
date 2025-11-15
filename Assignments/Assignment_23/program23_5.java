22b1c14f91ba24c7903920064fd22c65a3fe84ba
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-11-15 18:45:22 +0530

    Add Assignment_23 Q5

---
+++ Assignments/Assignment_23/program23_5.java
@@ -0,0 +1,99 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Packages
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+
+class program23_5
+{
+
+    public static void main(String args[]) 
+    {
+        int iSize = 0, iRet = 0;
+        
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+
+        iSize = sobj.nextInt();
+
+        ArrayX aobj = new ArrayX(iSize);
+
+        aobj.Accept();
+
+        iRet = aobj.Product();
+
+        System.out.println("Product of all elements in array is: " + iRet);
+        
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Class name:      ArrayX
+// Discription:     Class to allocate memory dynamically of array and accepts its elements from user
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Logic Functions of ArrayX:
+//
+// Function name:   Product
+// Discription:     Calculates the product of all elements in the array and returns it
+// Input:           Integer
+// Output:          Integer
+//
+// Author:          Rushikesh Vinod Gunthe
+// Date:            15/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ArrayX 
+{
+
+    private int Arr[];
+    private int iSize;
+
+    // Constructor for allocating resources
+    public ArrayX(int iNo) 
+    {
+        iSize = iNo;            //Size of array
+        Arr = new int[iSize];   //Dynamic memory allocation of array
+    }
+
+    //Method to accept elements of array (Getter)
+    public void Accept() 
+    {
+        int iCnt = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Elements of Array:");
+
+        for (iCnt = 0; iCnt < iSize; iCnt++) 
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+    }
+
+    // Method to to return product of all elements in the array
+    public int Product() 
+    {
+        int iCnt = 0, iResult = 0;
+
+        iResult = 1;
+
+        for (iCnt = 0; iCnt < iSize; iCnt++) 
+        {
+            iResult = iResult * Arr[iCnt];
+        }
+
+        return iResult;
+        
+    }
+
+}
