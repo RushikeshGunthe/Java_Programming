2ddc1f6213a9ed5202ec2b2758c3feb68b489448
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-11-23 20:33:44 +0530

    Add Assignment_30 Q3

---
+++ Assignments/Assignment_30/program30_3.java
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Packages
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Input:   iRow = 4     iCol = 4
+// Output:  $   *   *   *
+//          *   $   *   *
+//          *   *   $   *
+//          *   *   *   $
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+
+
+class program30_3
+{
+
+    public static void main(String args[]) 
+    {
+        int iValue1 = 0, iValue2 = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of Rows: ");
+        iValue1 = sobj.nextInt();
+        
+        System.out.println("Enter number of Columns: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+        
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Class name:      Pattern
+//
+// Logic Functions of Pattern class:
+//
+// Function name:   Display
+// Discription:     Display on screen from A to the count of number alphabet in capital 
+// Input:           Integer
+// Output:          None
+//
+// Author:          Rushikesh Vinod Gunthe
+// Date:            22/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern 
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        
+        if(iRow == iCol)
+        {
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    if(i == j)
+                    {
+                        System.out.print("$\t");
+                    }
+                    else
+                    {
+                        System.out.print("*\t");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        else
+        {
+            System.out.println("Both input should be the same!!");
+
+        }
+    }
+}
