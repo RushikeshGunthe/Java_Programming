f399942531d4d1fe25e2e2cf47742559db3df18d
Rushikesh Gunthe <guntherushi111@gmail.com>
2026-01-28 23:39:44 +0530

    Add Assignment_58 Q3

---
+++ Assignments/Assignment_58/program58_3.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class program58_2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // Accept directory name
+        System.out.print("Enter directory path: ");
+        String dirName = sc.nextLine();
+
+        File dir = new File(dirName);
+
+        if (dir.exists() && dir.isDirectory()) 
+        {
+            File[] files = dir.listFiles();
+
+            try 
+            {
+                FileWriter fw = new FileWriter("marvellous.txt");
+
+                if (files != null && files.length > 0) 
+                {
+                    for (File file : files) 
+                    {
+                        if (file.isFile()) 
+                        {  
+                            FileReader fr = new FileReader(file);
+                            
+                            int ch;
+
+                            while ((ch = fr.read()) != -1) 
+                            {
+                                fw.write(ch);
+                            }
+
+                            fr.close();
+                            fw.write(System.lineSeparator());
+                            fw.write(System.lineSeparator());
+                        }
+                    }
+                }
+
+                fw.close();
+                System.out.println("All file data written successfully into marvellous.txt");
+
+            } 
+            catch (IOException e) 
+            {
+                System.out.println("Error while writing to marvellous.txt");
+            }
+
+        } 
+        else 
+        {
+            System.out.println("Invalid directory path.");
+        }
+
+        sc.close();
+    }
+}
